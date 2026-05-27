4a375ccf9f712d7516f5f4004990b200745f24f0
Isaac Morales <imorsan@correo.ugr.es>
2026-05-27 05:41:18 +0100

    a

--- gcp/setup_single_node.sh
+++ gcp/setup_single_node.sh
@@ -15,7 +15,7 @@
 set -e
 
 PROJECT=$(gcloud config get-value project)
-ZONE="europe-southwest1"
+ZONE="europe-west9"
 INSTANCE_NAME="cdle-single-node"
 MACHINE_TYPE="n2-highmem-16"
 DISK_SIZE="500GB"
